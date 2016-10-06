2f4e4138e425401842f2e3daf5dc917d0307e5de
kevin <kevin@zanbato.com>
2016-10-06 10:21:43 -0700

    adding bookmark filtering

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,20 +12,60 @@ import { Tag } from '../tag.ts';
 export class DashboardComponent implements OnInit {
   bookmarks: Bookmark[];
   tags: Tag[] = [];
+  filteringTagNames: string[] = [];
 
   constructor(private bookmarkService: BookmarkService) { }
 
   getBookmarks(): void {
-    this.bookmarkService.getBookmarks().then(bookmarks => this.bookmarks = bookmarks);
+    this.bookmarkService.getBookmarks().then(bookmarks => {
+      this.bookmarks = bookmarks;
+      this.updateTags(bookmarks);
+    });
+  }
+
+  filteredBookmarks() {
+    if(this.bookmarks) {
+      return this.bookmarks.filter(bookmark => {
+        for(var i in this.filteringTagNames) {
+          const tagName = this.filteringTagNames[i];
+          if(bookmark.tags.indexOf(tagName) == -1) {
+            return false;
+          }
+        }
+        return true;
+      });
+    }
+    return [];
   }
 
   ngOnInit() {
     this.getBookmarks();
-    this.tags = [
-        {name: 'Entertainment'},
-        {name: 'News'},
-        {name: 'Tech'}
-    ];
+  }
+
+  toggleFilter(tag: Tag) {
+    const tagName = tag.name;
+    const index = this.filteringTagNames.indexOf(tagName);
+    if(index == -1) {
+      this.filteringTagNames.push(tagName);
+    } else {
+      this.filteringTagNames.splice(index, 1)
+    }
+  }
+
+  updateTags(bookmarks: Bookmark[]) {
+    var tagNameSet = {};
+    bookmarks.forEach(bookmark => {
+      bookmark.tags.forEach(tag => {
+        tagNameSet[tag] = 1;
+      });
+    });
+
+    var tagNames = [];
+    for(var name in tagNameSet) {
+      tagNames.push(name);
+    }
+    tagNames.sort();
+    this.tags = tagNames.map(name => {return {name: name};});
   }
 
 }
